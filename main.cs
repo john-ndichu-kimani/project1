22f5c29795c31c57e2f2fc0eb7526cf5e32d13a7
john ndichu <johnndichu104@gmail.com>
2024-05-07 16:43:42 +0300

    added csharp file

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("What's your name?");
+        string name = Console.ReadLine();
+
+        Console.WriteLine($"Hello, {name}! Welcome to C# programming.");
+    }
+}
